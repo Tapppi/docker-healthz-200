c731af4ac836a8247e505813a5d2779dd9c39564
Tapani Moilanen <moilanen.tapani@gmail.com>
2017-06-03 21:34:55 +0300

    feat: graceful server

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM busybox
 MAINTAINER Tapani Moilanen <moilanen.tapani@gmail.com>
 
 # Build server as in http://www.sohamkamani.com/blog/2016/11/22/docker-server-busybox-golang/
+# Graceful server code from
 
 COPY ./server /home/
 EXPOSE 80
--- server.go
+++ server.go
@@ -1,16 +1,74 @@
+
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Here be dragons!")
+type Server struct {
+	logger *log.Logger
+	mux    *http.ServeMux
+}
+
+func NewServer(options ...func(*Server)) *Server {
+	s := &Server{
+		logger: log.New(os.Stdout, "", 0),
+		mux:    http.NewServeMux(),
+	}
+
+	for _, f := range options {
+		f(s)
+	}
+
+	s.mux.HandleFunc("/healthz", s.index)
+
+	return s
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+func (s *Server) index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Here be dragons!"))
 }
 
 func main() {
-	http.HandleFunc("/healthz", handler)
-	fmt.Println("Server running...")
-	http.ListenAndServe(":8080", nil)
+	stop := make(chan os.Signal)
+
+	signal.Notify(stop, os.Interrupt)
+
+	logger := log.New(os.Stdout, "", 0)
+
+	addr := ":" + os.Getenv("PORT")
+	if addr == ":" {
+		addr = ":8080"
+	}
+
+	s := NewServer(func(s *Server) { s.logger = logger })
+
+	h := &http.Server{Addr: addr, Handler: s}
+
+	go func() {
+		logger.Printf("Listening on http://0.0.0.0%s\n", addr)
+
+		if err := h.ListenAndServe(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	<-stop
+
+	logger.Println("\nShutting down the server...")
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	h.Shutdown(ctx)
+
+	logger.Println("Server gracefully stopped")
 }
